54d43e9b22c245d7318932261c1bc6c94dee9323
abocideQ <AQ.18981745898@outlook.com>
2023-03-19 22:05:14 +0800

    ...

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Project/Qt/Qt5.12.10/Tools/mingw730_64/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
